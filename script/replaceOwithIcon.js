a713e69507e48d2c467e5f171630fb4d5201ec7b
etiennefraisse <127605717+etiennefraisse@users.noreply.github.com>
2023-03-20 20:41:41 +0100

    Update replaceOwithIcon.js
    updated to insert svg rather than glyphs

--- script/replaceOwithIcon.js
+++ script/replaceOwithIcon.js
@@ -3,35 +3,30 @@
 ////////////////////////////////////////////////////////////////////////////////////////
 
 const icons_codes = [
-'&#xe800;',
-'&#xe801;',
-'&#xe802;',
-'&#xe803;',
-'&#xe804;', /*cloche*/
-'&#xe805;',
-'&#xe806;',
-'&#xe807;',
-'&#xe808;',
-'&#xe809;', /*calendrier*/
-'&#xf17d;',
-'&#xf1b8;',
-'&#xf323;'
-];		
+1, /*ampoule*/
+2, /* boom*/
+3, /* étoile*/
+4, /* gribouille*/
+5 /* boule à facettes*/
+];
+
+	
 const random = function(){return Math.floor(Math.random() * icons_codes.length);	};
 $('.o').each(function() {
 	var thetext = $(this).html();
 	thetext.match(/o/gi).forEach(function(){
-		thetext = thetext.replace(/o/i,'<i class="ican">'+icons_codes[random()]+'</i>');
+		//thetext = thetext.replace(/o/i,'<i class="ican">'+icons_codes[random()]+'</i>');
+		thetext = thetext.replace(/o/i,'<img class="underlined" src="svg/test-0'+icons_codes[random()]+'.svg" />');
 	});
 	$(this).html(thetext);
 });
 $('.o-cloche').each(function() {
 	var thetext = $(this).html();
-	thetext = thetext.replace(/o/i,'<i class="ican">&#xe804;</i>');
+	thetext = thetext.replace(/o/i,'<img class="underlined" src="svg/test-05.svg" />');
 	$(this).html(thetext);
 });
 $('.o-cal').each(function() {
 	var thetext = $(this).html();
-	thetext = thetext.replace(/o/i,'<i class="ican">&#xe809;</i>');
+	thetext = thetext.replace(/o/i,'<img class="underlined" src="svg/test-03.svg" />');
 	$(this).html(thetext);
 });
